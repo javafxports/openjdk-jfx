685014fe6a85e00886c9404c126655f39feba3ca
Arunprasad Rajkumar <ararunprasad@gmail.com>
2018-03-29 00:31:15 +0530

    Qt-dev tools are no longer needed by OpenJFX (#48)

--- .ci/before_install.sh
+++ .ci/before_install.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 if [[ "${TRAVIS_OS_NAME}" == osx ]]; then
   brew update
   brew install findutils
-  brew install qt5
-  brew link qt5 --force
   brew cask reinstall java
   brew outdated gradle || brew upgrade gradle
   brew unlink python # fixes 'run_one_line' is not defined error in backtrace
