99f62c70d59ad3d211d5469d4e9ad585d17b72e0
Michael Ennen <mike.ennen@gmail.com>
2018-05-24 16:08:30 -0700

    Use OpenJDK instead of Oracle JDK for CI.
    Fixes #86

--- .ci/before_install.sh
+++ .ci/before_install.sh
@@ -2,9 +2,15 @@
 set -euo pipefail
 
 if [[ "${TRAVIS_OS_NAME}" == osx ]]; then
+  openjdk='openjdk-10.0.1_osx-x64_bin.tar.gz'
+  wget https://download.java.net/java/GA/jdk10/10.0.1/fb4372174a714e6b8c52526dc134031e/10/$openjdk
+  mkdir -p ~/jdk10
+  tar -zxf "$openjdk" -C ~/jdk10 --strip-components 4 # Strip Contents/Home/
+  ls -la ~/jdk10
   brew update
   brew install findutils
-  brew cask reinstall java
+  # brew tap AdoptOpenJDK/openjdk
+  # brew install adoptopenjdk-openjdk10
   brew unlink python # fixes 'run_one_line' is not defined error in backtrace
 fi
 
