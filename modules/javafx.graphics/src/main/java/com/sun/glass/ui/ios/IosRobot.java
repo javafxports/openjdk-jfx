brcolow/openjfx

Move Robot to public API.

modules/javafx.graphics/src/main/java/com/sun/glass/ui/ios/IosRobot.java
@@ -25,8 +25,16 @@
 
 package com.sun.glass.ui.ios;
 
-import com.sun.glass.ui.*;
+import com.sun.glass.ui.Application;
+import com.sun.glass.ui.GlassRobot;
+
 import java.nio.IntBuffer;
+
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.scene.paint.Color;
+import javafx.scene.robot.Robot;
 
 /**
  * iOS platform implementation class of test automation Robot.
@@ -37,38 +45,39 @@ final class IosRobot extends Robot {
 
     // init and create native robot object
     private native long _init();
-    @Override protected void _create() {
+
+    @Override public void create() {
         ptr = _init();
     }
 
     // release native robot object
     private native void _destroy(long ptr);
-    @Override protected void _destroy() {
+    @Override public void destroy() {
         _destroy(ptr);
         ptr = 0;
     }
 
     // synthesize key press
     private native void _keyPress(long ptr, int code);
-    @Override protected void _keyPress(int code) {
+    @Override public void keyPress(KeyCode code) {
         if (ptr == 0) {
             return;
         }
-        _keyPress(ptr, code);
+        _keyPress(ptr, code.getCode());
     }
 
     // synthesize key release
     private native void _keyRelease(long ptr, int code);
-    @Override protected void _keyRelease(int code) {
+    @Override public void keyRelease(KeyCode code) {
         if (ptr == 0) {
             return;
         }
-        _keyRelease(ptr, code);
+        _keyRelease(ptr, code.getCode());
     }
 
     // synthesize mouse motion
     private native void _mouseMove(long ptr, int x, int y);
-    @Override protected void _mouseMove(int x, int y) {
+    @Override public void mouseMove(int x, int y) {
         if (ptr == 0) {
             return;
         }
@@ -77,24 +86,24 @@ final class IosRobot extends Robot {
 
     // synthesize mouse press of buttons
     private native void _mousePress(long ptr, int buttons);
-    @Override protected void _mousePress(int buttons) {
+    @Override public void mousePress(MouseButton button) {
         if (ptr == 0) {
             return;
         }
-        _mousePress(ptr, buttons);
+        _mousePress(ptr, GlassRobot.convertToRobotMouseButton(button));
     }
 
     // synthesize mouse release of buttons
     private native void _mouseRelease(long ptr, int buttons);
-    @Override protected void _mouseRelease(int buttons) {
+    @Override public void mouseRelease(MouseButton button) {
         if (ptr == 0) {
             return;
         }
-        _mouseRelease(ptr, buttons);
+        _mouseRelease(ptr, GlassRobot.convertToRobotMouseButton(button));
     }
 
     private native void _mouseWheel(long ptr, int wheelAmt);
-    @Override protected void _mouseWheel(int wheelAmt) {
+    @Override public void mouseWheel(int wheelAmt) {
         if (ptr == 0) {
             return;
         }
@@ -103,7 +112,7 @@ final class IosRobot extends Robot {
 
     // get x-coordinate of mouse location
     private native int _getMouseX(long ptr);
-    @Override protected int _getMouseX() {
+    @Override public int getMouseX() {
         if (ptr == 0) {
             return 0;
         }
@@ -112,7 +121,7 @@ final class IosRobot extends Robot {
 
     // get x-coordinate of mouse location
     private native int _getMouseY(long ptr);
-    @Override protected int _getMouseY() {
+    @Override public int getMouseY() {
         if (ptr == 0) {
             return 0;
         }
@@ -120,22 +129,29 @@ final class IosRobot extends Robot {
     }
 
     private native int _getPixelColor(long ptr, int x, int y);
-    @Override protected int _getPixelColor(int x, int y) {
+    @Override public Color getPixelColor(int x, int y) {
         if (ptr == 0) {
-            return 0;
+            return GlassRobot.convertFromIntArgb(0);
         }
-        return _getPixelColor(ptr, x, y);
+        return GlassRobot.convertFromIntArgb(_getPixelColor(ptr, x, y));
+    }
+
+    @Override
+    public void getScreenCapture(int x, int y, int width, int height, int[] data) {
+        _getScreenCapture(ptr, x, y, width, height, data);
     }
 
     // capture bitmap image of (x, y, x + width, y + height) area
     native private void _getScreenCapture(long ptr, int x, int y, int width, int height, int[] data);
-    @Override protected Pixels _getScreenCapture(int x, int y, int width, int height, boolean isHiDPI) {
+
+    @Override protected Image getScreenCapture(int x, int y, int width, int height, boolean isHiDPI) {
         if (ptr == 0) {
             return null;
         }
         int data[] = new int[width * height];
         _getScreenCapture(ptr, x, y, width, height, data);
-        return Application.GetApplication().createPixels(width, height, IntBuffer.wrap(data));
+        return GlassRobot.convertFromPixels(Application.GetApplication().createPixels(
+                width, height, IntBuffer.wrap(data)));
     }
 }
 

javafxports/openjdk-jfx

modules/javafx.graphics/src/main/java/com/sun/glass/ui/ios/IosRobot.java
@@ -25,76 +25,91 @@
 
 package com.sun.glass.ui.ios;
 
-import com.sun.glass.ui.*;
-import java.nio.IntBuffer;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.scene.paint.Color;
+
+import com.sun.glass.ui.Application;
+import com.sun.glass.ui.GlassRobot;
 
 /**
  * iOS platform implementation class of test automation Robot.
  */
-final class IosRobot extends Robot {
+final class IosRobot extends GlassRobot {
 
     private long ptr = 0;
 
     // init and create native robot object
     private native long _init();
-    @Override protected void _create() {
+
+    @Override public void create() {
+        Application.checkEventThread();
         ptr = _init();
     }
 
     // release native robot object
     private native void _destroy(long ptr);
-    @Override protected void _destroy() {
+    @Override public void destroy() {
+        Application.checkEventThread();
         _destroy(ptr);
         ptr = 0;
     }
 
     // synthesize key press
     private native void _keyPress(long ptr, int code);
-    @Override protected void _keyPress(int code) {
+    @Override public void keyPress(KeyCode code) {
+        Application.checkEventThread();
         if (ptr == 0) {
             return;
         }
-        _keyPress(ptr, code);
+        _keyPress(ptr, code.getCode());
     }
 
     // synthesize key release
     private native void _keyRelease(long ptr, int code);
-    @Override protected void _keyRelease(int code) {
+    @Override public void keyRelease(KeyCode code) {
+        Application.checkEventThread();
         if (ptr == 0) {
             return;
         }
-        _keyRelease(ptr, code);
+        _keyRelease(ptr, code.getCode());
     }
 
     // synthesize mouse motion
     private native void _mouseMove(long ptr, int x, int y);
-    @Override protected void _mouseMove(int x, int y) {
+    @Override public void mouseMove(double x, double y) {
+        Application.checkEventThread();
         if (ptr == 0) {
             return;
         }
-        _mouseMove(ptr, x, y);
+        _mouseMove(ptr, (int) x, (int) y);
     }
 
     // synthesize mouse press of buttons
     private native void _mousePress(long ptr, int buttons);
-    @Override protected void _mousePress(int buttons) {
+    @Override
+    public void mousePress(MouseButton... buttons) {
+        Application.checkEventThread();
         if (ptr == 0) {
             return;
         }
-        _mousePress(ptr, buttons);
+        _mousePress(ptr, GlassRobot.convertToRobotMouseButton(buttons));
     }
 
     // synthesize mouse release of buttons
     private native void _mouseRelease(long ptr, int buttons);
-    @Override protected void _mouseRelease(int buttons) {
+    @Override
+    public void mouseRelease(MouseButton... buttons) {
+        Application.checkEventThread();
         if (ptr == 0) {
             return;
         }
-        _mouseRelease(ptr, buttons);
+        _mouseRelease(ptr, GlassRobot.convertToRobotMouseButton(buttons));
     }
 
     private native void _mouseWheel(long ptr, int wheelAmt);
-    @Override protected void _mouseWheel(int wheelAmt) {
+    @Override public void mouseWheel(int wheelAmt) {
+        Application.checkEventThread();
         if (ptr == 0) {
             return;
         }
@@ -103,7 +118,8 @@ final class IosRobot extends Robot {
 
     // get x-coordinate of mouse location
     private native int _getMouseX(long ptr);
-    @Override protected int _getMouseX() {
+    @Override public double getMouseX() {
+        Application.checkEventThread();
         if (ptr == 0) {
             return 0;
         }
@@ -112,7 +128,8 @@ final class IosRobot extends Robot {
 
     // get x-coordinate of mouse location
     private native int _getMouseY(long ptr);
-    @Override protected int _getMouseY() {
+    @Override public double getMouseY() {
+        Application.checkEventThread();
         if (ptr == 0) {
             return 0;
         }
@@ -120,22 +137,23 @@ final class IosRobot extends Robot {
     }
 
     private native int _getPixelColor(long ptr, int x, int y);
-    @Override protected int _getPixelColor(int x, int y) {
+    @Override public Color getPixelColor(double x, double y) {
+        Application.checkEventThread();
         if (ptr == 0) {
-            return 0;
+            return GlassRobot.convertFromIntArgb(0);
         }
-        return _getPixelColor(ptr, x, y);
+        return GlassRobot.convertFromIntArgb(_getPixelColor(ptr, (int) x, (int) y));
     }
 
-    // capture bitmap image of (x, y, x + width, y + height) area
-    native private void _getScreenCapture(long ptr, int x, int y, int width, int height, int[] data);
-    @Override protected Pixels _getScreenCapture(int x, int y, int width, int height, boolean isHiDPI) {
+    private native void _getScreenCapture(long ptr, int x, int y, int width, int height, int[] data);
+    @Override
+    public void getScreenCapture(int x, int y, int width, int height, int[] data, boolean scaleToFit) {
+        Application.checkEventThread();
         if (ptr == 0) {
-            return null;
+            return;
         }
-        int data[] = new int[width * height];
         _getScreenCapture(ptr, x, y, width, height, data);
-        return Application.GetApplication().createPixels(width, height, IntBuffer.wrap(data));
     }
+
 }
 