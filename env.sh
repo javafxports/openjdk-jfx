c79b9e3603d72507412dfd5a203c967c61cdba6a
Gregery Barton <dean@gregery.id.au>
2018-07-27 09:43:40 +1000

    use msv community

---
+++ env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /cygdrive/c/Users/Gregery\ Barton/Documents/GitHub/openjdk-jfx/
+
+
+
+export WINSDK_DIR="C:/Program Files (x86)/Windows Kits/10"
+export WINSDKPATH="C:/Program Files (x86)/Windows Kits/10"
+export WINDOWS_SDK_DIR="C:/Program Files (x86)/Windows Kits/10"
+
+export VS150COMNTOOLS="C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Auxiliary/Build"
+
+export DXSDK_DIR="C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)"
+
+# ANT_HOME causes build failure if it has spaces.
+export ANT_HOME="C:/Progra~1/Java/apache-ant-1.8.2"
+export MSVC_VER=14.14.26428
+
+export PATH=$PATH:"/cygdrive/c/Program Files/Java/gradle-4.6/bin":"/cygdrive/c/Program Files/Java/apache-ant-1.8.2/bin"
