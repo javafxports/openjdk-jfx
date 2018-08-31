2f160edb35c4e3913a2de1d4da6f706ac4287910
Kevin Rushforth <kevin.rushforth@oracle.com>
2018-08-31 06:51:36 -0700

    Run checkWhiteSpace without -v option

--- .github/pr-check-whitespace.sh
+++ .github/pr-check-whitespace.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 
-echo ""
-echo "Checking for white space errors"
-echo ""
-
 if [ -z "$TRAVIS_BRANCH" ]; then
     echo "TRAVIS_BRANCH is undefined, skipping white-space check"
     exit 0
@@ -12,6 +8,16 @@ fi
 ROOTDIR=$(git rev-parse --show-toplevel)
 cd $ROOTDIR
 
-# Check all files changed as part of this pull request
-# FIXME: maybe remove the -v option at some point
-bash .github/pr-list-files.sh | bash tools/scripts/checkWhiteSpace -S -x -v
+echo ""
+echo "--- List of added or modified files"
+echo ""
+
+# List all files that are part of this pull request
+bash .github/pr-list-files.sh
+
+echo ""
+echo "--- Checking for white space errors"
+echo ""
+
+# Check all files that are part of this pull request
+bash .github/pr-list-files.sh | bash tools/scripts/checkWhiteSpace -S -x
