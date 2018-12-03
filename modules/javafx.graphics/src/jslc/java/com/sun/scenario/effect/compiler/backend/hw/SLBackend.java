e463eb317321238b765546c8bbed7e6df6669d38
Dell Green <dell.green@ideaworks.co.uk>
2018-12-03 14:57:33 +0000

    8167068: Fixed undefined pixcoord error starting javafx

--- modules/javafx.graphics/src/jslc/java/com/sun/scenario/effect/compiler/backend/hw/SLBackend.java
+++ modules/javafx.graphics/src/jslc/java/com/sun/scenario/effect/compiler/backend/hw/SLBackend.java
@@ -81,8 +81,20 @@ public abstract class SLBackend extends TreeScanner {
         sb.append(s);
     }
 
+
+    static String PIXCOORD = "vec2 pixcoord = vec2(\n"+
+        "    gl_FragCoord.x-jsl_pixCoordOffset.x,\n" +
+        "    ((jsl_pixCoordOffset.z-gl_FragCoord.y)*jsl_pixCoordOffset.w)-jsl_pixCoordOffset.y);\n";
+
+    static String MAIN = "void main() {\n";
+
+
     public final String getShader() {
-        return getHeader() + sb.toString();
+        String answer = getHeader() + sb.toString();
+        if (isPixcoordReferenced) {
+            answer = answer.replace(MAIN, MAIN + PIXCOORD);
+        }
+        return answer;
     }
 
     protected final JSLParser getParser() {
