a747a0804e3ea1f1afbdd68b9967e5cc9ed7724e
Michael Ennen <mike.ennen@gmail.com>
2019-01-26 23:31:40 -0700

    fixup!

--- modules/javafx.graphics/src/jslc/java/com/sun/scenario/effect/compiler/model/Types.java
+++ modules/javafx.graphics/src/jslc/java/com/sun/scenario/effect/compiler/model/Types.java
@@ -12,9 +12,9 @@ public class Types {
     public static final Type INT3 = new Type(BaseType.INT, "int3", 3);
     public static final Type INT4 = new Type(BaseType.INT, "int4", 4);
     public static final Type BOOL = new Type(BaseType.BOOL, "bool", 1);
-    public static final Type BOOL2 = new Type(BaseType.BOOL, "bool2", 1);
-    public static final Type BOOL3 = new Type(BaseType.BOOL, "bool3", 1);
-    public static final Type BOOL4 = new Type(BaseType.BOOL, "bool4", 1);
+    public static final Type BOOL2 = new Type(BaseType.BOOL, "bool2", 2);
+    public static final Type BOOL3 = new Type(BaseType.BOOL, "bool3", 3);
+    public static final Type BOOL4 = new Type(BaseType.BOOL, "bool4", 4);
     public static final Type SAMPLER = new Type(BaseType.SAMPLER, "sampler", 1);
     public static final Type LSAMPLER = new Type(BaseType.SAMPLER, "lsampler", 1);
     public static final Type FSAMPLER = new Type(BaseType.SAMPLER, "fsampler", 1);
