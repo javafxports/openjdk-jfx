b73a050cb157035f8efcaaca4112d116be477706
Arunprasad Rajkumar <arunprasad.rajkumar@oracle.com>
2019-03-02 07:43:08 +0530

    Unify postive and negative tests

--- modules/javafx.web/src/test/java/test/javafx/scene/web/SubresourceIntegrityTest.java
+++ modules/javafx.web/src/test/java/test/javafx/scene/web/SubresourceIntegrityTest.java
@@ -38,49 +38,52 @@ import org.junit.runners.Parameterized.Parameters;
 import org.junit.runners.Parameterized;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assume.assumeTrue;
 
 @RunWith(Parameterized.class)
 public final class SubresourceIntegrityTest extends TestBase {
 
     // To arguments from junit data provider.
     private final String hashValue;
-    private final String algorithm;
+    private final String expected;
     private File htmlFile;
-    // Only sha256, sha384, sha512 are supported
-    // Ref. https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity#Using_Subresource_Integrity
-    private static Set<String> ALLOWED_HASH_ALGOS = Set.of("sha256", "sha384", "sha512");
-
+    // Expectations
+    private final static String LOADED = "hello";
+    private final static String NOT_LOADED = "not loaded";
     // TODO: junit 4.11 provides an option to label arguments.
     @Parameters
     public static Collection<String[]> data() {
         return Arrays.asList(new String[][] {
             // shasum -b -a 1 subresource-integrity-test.js | awk '{ print $1 }' | xxd -r -p | base64
-            {"sha1-/kpzvnGzRkcE9OFn5j8qRE61nZY="},
+            {"sha1-/kpzvnGzRkcE9OFn5j8qRE61nZY=", LOADED},
             // shasum -b -a 224 subresource-integrity-test.js | awk '{ print $1 }' | xxd -r -p | base64
-            {"sha224-zgiBbbuKJixMVEkaOXnvpSYZGsx7SbSZ0QOckg=="},
+            {"sha224-zgiBbbuKJixMVEkaOXnvpSYZGsx7SbSZ0QOckg==", LOADED},
             // shasum -b -a 256 subresource-integrity-test.js | awk '{ print $1 }' | xxd -r -p | base64
-            {"sha256-vcl3cFaIDAtcQBkUZFdY+tW/bjrg6vX1R+hQ8uB5tHc="},
+            {"sha256-vcl3cFaIDAtcQBkUZFdY+tW/bjrg6vX1R+hQ8uB5tHc=", LOADED},
             // shasum -b -a 384 subresource-integrity-test.js | awk '{ print $1 }' | xxd -r -p | base64
-            {"sha384-+GrI+cacF05VlQitRghQhs1by9CSIyc8XgZTbymUg2oA0EYdLiPMtilnFP3LDbkY"},
+            {"sha384-+GrI+cacF05VlQitRghQhs1by9CSIyc8XgZTbymUg2oA0EYdLiPMtilnFP3LDbkY", LOADED},
             // shasum -b -a 512 subresource-integrity-test.js | awk '{ print $1 }' | xxd -r -p | base64
-            {"sha512-V8m3j61x5soaVcO83NuHavY7Yn4MQYoUgrqJe38f6QYG9QzzgWbVDB1SrZsZ2CVR1IsOnV2MLhnDaZhWOwHDsw=="},
-            // SubresourceIntegrity is not tested for sha1 and sha224
-            {"sha1-0000000000000000000000000000"},
-            {"sha224-0000000000000000000000000000000000000000"},
+            {"sha512-V8m3j61x5soaVcO83NuHavY7Yn4MQYoUgrqJe38f6QYG9QzzgWbVDB1SrZsZ2CVR1IsOnV2MLhnDaZhWOwHDsw==", LOADED},
+            // Only sha256, sha384, sha512 are validated, rest will be ignored and loaded
+            // Ref. https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity#Using_Subresource_Integrity
+            {"sha1-0000000000000000000000000000", LOADED},
+            {"sha224-0000000000000000000000000000000000000000", LOADED},
+            // negative tests, change the hash value and ensure it fails
+            {"sha256-Vcl3cFaIDAtcQBkUZFdY+tW/bjrg6vX1R+hQ8uB5tHc=", NOT_LOADED},
+            {"sha384-+grI+cacF05VlQitRghQhs1by9CSIyc8XgZTbymUg2oA0EYdLiPMtilnFP3LDbkY", NOT_LOADED},
+            {"sha512-v8m3j61x5soaVcO83NuHavY7Yn4MQYoUgrqJe38f6QYG9QzzgWbVDB1SrZsZ2CVR1IsOnV2MLhnDaZhWOwHDsw==", NOT_LOADED},
+            // should load for invalid hash algorithm
+            {"unknown-0000", LOADED},
+            {"", LOADED},
         });
     }
 
-    private static boolean allowed(final String algorithm) {
-        return ALLOWED_HASH_ALGOS.contains(algorithm);
-    }
-
-    public SubresourceIntegrityTest(final String hashValue) {
+    public SubresourceIntegrityTest(final String hashValue, final String expected) {
         this.hashValue = hashValue;
-        this.algorithm = hashValue.substring(0, hashValue.indexOf('-'));
+        this.expected = expected;
     }
 
-    public void setup(final String hashValue) throws Exception {
+    @Before
+    public void setup() throws Exception {
         // loadContent won't work with CORS, use file:// for main resource.
         htmlFile = new File("subresource-integrity-test.html");
         final FileOutputStream out = new FileOutputStream(htmlFile);
@@ -89,36 +92,23 @@ public final class SubresourceIntegrityTest extends TestBase {
         final String html =
                 String.format("<html>\n" +
                 "<head><script src='%s' integrity='%s' crossorigin='anonymous'></script></head>\n" +
-                "</html>", scriptUrl, hashValue);
+                "<body>%s</body>\n" +
+                "</html>", scriptUrl, hashValue, NOT_LOADED);
         out.write(html.getBytes());
         out.close();
     }
 
     @Test
-    public void testScriptTagWithCorrectHashValue() throws Exception {
-        setup(hashValue);
+    public void testScriptTagWithCorrectHashValue() {
         load(htmlFile);
         final String bodyText = (String) executeScript("document.body.innerText");
-        assertNotNull("document.body.innerText must be non null for " + algorithm, bodyText);
-        assertEquals("hello text must be added to body if script loaded successfully for " + algorithm, "hello", bodyText);
-    }
-
-    @Test
-    public void testScriptTagWithIncorrectHashValue() throws Exception {
-        // SubresourceIntegrity is not tested for sha1 and sha224.
-        assumeTrue(allowed(algorithm));
-        // corrupt the hash value to check the negative case
-        char c = hashValue.charAt(20);
-        setup(hashValue.replace(c, (char) (c + 1)));
-        load(htmlFile);
-        final String bodyText = (String) executeScript("document.body.innerText");
-        assertNotNull("document.body.innerText must be non null for" + algorithm, bodyText);
-        assertEquals("body text should be empty for " + algorithm, "", bodyText);
+        assertNotNull("document.body.innerText must be non null for " + hashValue, bodyText);
+        assertEquals(hashValue, expected, bodyText);
     }
 
     @After
     public void tearDown() {
-        if (htmlFile != null && !htmlFile.delete()) {
+        if (!htmlFile.delete()) {
             htmlFile.deleteOnExit();
         }
     }
