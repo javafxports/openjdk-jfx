bb74870e4a28869999a46927f30ab8430fcb418d
Thiago M Sayao <thiago.sayao@gmail.com>
2019-04-15 21:35:31 -0300

    Move the fix to WindowContextTop where it's more suitable.

--- modules/javafx.graphics/src/main/native-glass/gtk/glass_window.cpp
+++ modules/javafx.graphics/src/main/native-glass/gtk/glass_window.cpp
@@ -570,11 +570,6 @@ void WindowContextBase::reparent_children(WindowContext* parent) {
 void WindowContextBase::set_visible(bool visible) {
     if (visible) {
         gtk_widget_show_all(gtk_widget);
-
-        //JDK-8220272 - fire event first because GDK_FOCUS_CHANGE is not always in order
-        if(jwindow && isEnabled()) {
-            mainEnv->CallVoidMethod(jwindow, jWindowNotifyFocus, com_sun_glass_events_WindowEvent_FOCUS_GAINED);
-        }
     } else {
         gtk_widget_hide(gtk_widget);
         if (jview && is_mouse_entered) {
@@ -1166,6 +1161,10 @@ void WindowContextTop::set_visible(bool visible)
         }
     }
     WindowContextBase::set_visible(visible);
+    //JDK-8220272 - fire event first because GDK_FOCUS_CHANGE is not always in order
+    if (visible && jwindow && isEnabled()) {
+        mainEnv->CallVoidMethod(jwindow, jWindowNotifyFocus, com_sun_glass_events_WindowEvent_FOCUS_GAINED);
+    }
 }
 
 void WindowContextTop::set_bounds(int x, int y, bool xSet, bool ySet, int w, int h, int cw, int ch) {
