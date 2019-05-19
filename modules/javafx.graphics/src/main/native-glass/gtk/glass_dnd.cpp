e089e887a22745443ea9ba93428a0ad4ede98eaf
Thiago M Sayao <thiago.sayao@gmail.com>
2019-05-19 16:40:18 -0300

    possible fix for 3.2.0+ (don't like it, could not find a better one)

--- modules/javafx.graphics/src/main/native-glass/gtk/glass_dnd.cpp
+++ modules/javafx.graphics/src/main/native-glass/gtk/glass_dnd.cpp
@@ -820,6 +820,10 @@ static void process_dnd_source_selection_req(GdkWindow *window, GdkEvent *gdkEve
 
     gdk_selection_send_notify(event->requestor, event->selection, event->target,
                               (is_data_set) ? event->property : GDK_NONE, event->time);
+
+#if GTK_CHECK_VERSION(3, 2, 0)
+    gdk_threads_add_idle((GSourceFunc) dnd_finish_callback, NULL);
+#endif
 }
 
 static void process_dnd_source_mouse_release(GdkWindow *window, GdkEvent *event) {
