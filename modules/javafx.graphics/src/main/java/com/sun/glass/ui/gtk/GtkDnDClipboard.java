cf0cee2083d15adbdf8792b7f88643ef2b24950e
Thiago Milczarek Sayão <thiago.sayao@clamed.com.br>
2019-06-13 13:18:38 -0300

    Aha whitespaces! got you!

--- modules/javafx.graphics/src/main/java/com/sun/glass/ui/gtk/GtkDnDClipboard.java
+++ modules/javafx.graphics/src/main/java/com/sun/glass/ui/gtk/GtkDnDClipboard.java
@@ -26,38 +26,40 @@ package com.sun.glass.ui.gtk;
 
 import com.sun.glass.ui.Clipboard;
 import com.sun.glass.ui.SystemClipboard;
-
 import java.util.HashMap;
 
-final class GtkDnDClipboard extends SystemClipboard {
-    
+final class GtkDnDClipboard extends SystemClipboard{
+
     public GtkDnDClipboard() {
         super(Clipboard.DND);
     }
-    
-    protected void pushToSystem(HashMap<String, Object> cacheData, int supportedActions) {
-        final int performedAction = pushToSystemImpl(cacheData, supportedActions);
-        
+
+    protected void pushToSystem(HashMap<String, Object> cacheData,
+                                int supportedActions) {
+        final int performedAction = pushToSystemImpl(cacheData,
+                                                     supportedActions);
+
         // drag can fail or be cancelled
-        if (performedAction != Clipboard.ACTION_NONE) {
+        if (performedAction != ACTION_NONE) {
             actionPerformed(performedAction);
         }
     }
-    
+
     @Override
     protected native boolean isOwner();
-    
+
     protected native int pushToSystemImpl(HashMap<String, Object> cacheData, int supportedActions);
-    
+
     @Override
     protected native void pushTargetActionToSystem(int actionDone);
-    
+
     @Override
     protected native Object popFromSystem(String mimeType);
-    
+
     @Override
     protected native int supportedSourceActionsFromSystem();
-    
+
     @Override
     protected native String[] mimesFromSystem();
+
 }
